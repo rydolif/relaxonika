ed6122b56c7e019c347452edcb87a2e0647fd84c
master <rudolifrudolif@gmail.com>
2020-10-12 14:02:23 +0300

    start + tabs + present

--- app/js/app.js
+++ app/js/app.js
@@ -124,41 +124,46 @@ document.addEventListener("DOMContentLoaded", function() {
 			});
 		};
 		// scrollTo('.header__nav');
-	
-	//----------------------FIXED-HEADER-----------------------
-		const headerFixed = (headerFixed, headerActive) => {
-			const header =  document.querySelector(headerFixed),
-						active = headerActive.replace(/\./, '');
-	
-			window.addEventListener('scroll', function() {
-				const top = pageYOffset;
-				
-				if (top >= 90) {
-					header.classList.add(active);
-				} else {
-					header.classList.remove(active);
+
+	//----------------------TABS-JS----------------------
+		const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+			const header = document.querySelector(headerSelector),
+						tab = document.querySelectorAll(tabSelector),
+						content = document.querySelectorAll(contentSelector);
+
+			function hideTabContent() {
+				content.forEach(item => {
+					item.style.display = "none";
+				});
+
+				tab.forEach(item => {
+					item.classList.remove(activeClass);
+				});
+			}
+
+			function showTabContent(i = 0) {
+				content[i].style.display = "flex";
+				tab[i].classList.add(activeClass);
+			}
+
+			hideTabContent();
+			showTabContent();
+
+			header.addEventListener('click', (e) => {
+				const target = e.target;
+				if (target && 
+					(target.classList.contains(tabSelector.replace(/\./, '')) || 
+					target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
+					tab.forEach((item, i) => {
+						if (target == item || target.parentNode == item) {
+							hideTabContent();
+							showTabContent(i);
+						}
+					});
 				}
-	
 			});
-	
 		};
-		// headerFixed('.header', '.header--active');
-	
-	//----------------------HAMBURGER-----------------------
-		const hamburger = (hamburgerButton, hamburgerNav, hamburgerHeader) => {
-			const button = document.querySelector(hamburgerButton),
-						nav = document.querySelector(hamburgerNav),
-						header = document.querySelector(hamburgerHeader);
-	
-			button.addEventListener('click', (e) => {
-				button.classList.toggle('hamburger--active');
-				nav.classList.toggle('header__nav--active');
-				header.classList.toggle('header--menu');
-			});
-	
-		};
-		// hamburger('.hamburger', '.header__nav', '.header');
-	
+		tabs('.tabs', '.tabs__item', '.tabs__wrap', 'active');
 	
 	});
 	
